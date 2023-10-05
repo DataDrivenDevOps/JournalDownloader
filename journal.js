f43c30314d7fe74509feeabaae7dc2abfa3a2643
DataDriven <147101298+DataDrivenDevOps@users.noreply.github.com>
2023-10-05 17:44:33 -0400

    Update journal.js
    removed git since the ROI wouldn't be worth it

--- journal.js
+++ journal.js
@@ -34,7 +34,6 @@ async function downloadJournal(journalNumber) {
 
       if (failedDownloads >= 3) {
         console.error('Too many failed downloads. Stopping script.');
-        pushToGitHub();
         process.exit(1);
       }
 
@@ -60,23 +59,10 @@ async function downloadJournal(journalNumber) {
 
     if (failedDownloads >= 3) {
       console.error('Too many failed downloads. Stopping script.');
-      pushToGitHub();
       process.exit(1);
     }
   }
 }
-
-/* Putting on hold for now... 
-function pushToGitHub() {
-  const git = simpleGit();
-
-  git.add('.')
-    .then(() => git.commit('Downloaded journals'))
-    .then(() => git.push())
-    .then(() => console.log('Changes pushed to GitHub'))
-    .catch(error => console.error(`Error: Failed to push changes to GitHub with message ${error.message}`));
-}
-*/
 
 async function downloadJournals() {
   for (let i = 1; i <= 1000; i++) {
@@ -88,24 +74,20 @@ async function downloadJournals() {
   }
 
   console.log(`Script finished with ${successfulDownloads} successful downloads, ${skippedDownloads} skipped downloads, and ${failedDownloads} failed downloads.`);
-  pushToGitHub();
 }
 
 downloadJournals();
 
 process.on('uncaughtException', error => {
   console.error(`Uncaught exception: ${error.message}`);
-  pushToGitHub();
   process.exit(1);
 });
 
 process.on('SIGINT', () => {
   console.log('Script stopped by user.');
-  pushToGitHub();
   process.exit(0);
 });
 
 process.on('exit', () => {
   console.log(`Script finished with ${successfulDownloads} successful downloads, ${skippedDownloads} skipped downloads, and ${failedDownloads} failed downloads.`);
-  pushToGitHub();
 });
